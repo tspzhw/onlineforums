a4e591c9bed1bf6a40246f5b5121fb001808138d
tspzhw <2392221684@qq.com>
2022-02-14 16:30:46 +0800

    init repo

---
+++ src/main/java/com/tsp/onlineforums/OnlineforumsApplication.java
@@ -0,0 +1,13 @@
+package com.tsp.onlineforums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineforumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineforumsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsp/onlineforums/OnlineforumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsp.onlineforums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineforumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
